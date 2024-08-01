88409823bfcc13f84102433a27dcef90e367a4f2
cici-cmd <cibelesoares79@gmail.com>
2024-08-01 00:59:54 -0300

    Delete mecajato-master directory

--- mecajato-master/clientes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Carro
-
-admin.site.register(Cliente)
-admin.site.register(Carro)
--- mecajato-master/servicos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CategoriaManutencao, Servico, ServicoAdicional
-
-admin.site.register(CategoriaManutencao)
-admin.site.register(Servico)
-admin.site.register(ServicoAdicional)
